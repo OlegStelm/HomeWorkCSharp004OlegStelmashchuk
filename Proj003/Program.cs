6e7d1406f08f25c9526a029b9338f3a55888424c
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-11 16:44:27 +0300

    AddProj003

---
+++ Proj003/Program.cs
@@ -0,0 +1,19 @@
+﻿/*  **Задача 29**
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. */
+
+int[] array = new int[8];
+char[] arrayUTF8 = new char[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(10);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine($"");
+for (int j = 0; j < arrayUTF8.Length; j++)
+{
+    int b;
+    b = new Random().Next(97, 122);
+    char cb = Convert.ToChar(b);
+    arrayUTF8[j] = cb;
+    Console.Write($"{arrayUTF8[j]} ");
+}
